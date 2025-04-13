07780f4e8620d42292f89a785b0b50194c6aed0f
Shyam Gupta <shyamgupta4004@gmail.com>
2025-04-13 23:59:13 +0530

    Added Group Anagrams

--- Array/Group Anagrams.cpp
+++ Array/Group Anagrams.cpp
@@ -19,3 +19,23 @@ public:
         return result;
     }
 };
+
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+
+        map<string, vector<string>> mp;
+        for (string s : strs)
+        {
+            string temp = s;
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto x : mp)
+            result.push_back(x.second);
+        return result;
+    }
+};
