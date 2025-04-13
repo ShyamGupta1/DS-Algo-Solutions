3400db6a562dbd06e60bd73eb00e7c4a05f0d679
Shyam Gupta <shyamgupta4004@gmail.com>
2025-04-13 23:49:52 +0530

    Added Group Anagrams

---
+++ Array/Group Anagrams.cpp
@@ -0,0 +1,21 @@
+// T.C: O(N∗M∗Log(M))  M-> Length of each String, N -> Total String
+// S.C: O(M*N)
+
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+
+        vector<string> copy = strs;
+        for (string &s : copy)
+            sort(s.begin(), s.end());
+        map<string, vector<string>> mp;
+        for (int i = 0; i < copy.size(); i++)
+            mp[copy[i]].push_back(strs[i]);
+        vector<vector<string>> result;
+        for (auto x : mp)
+            result.push_back(x.second);
+        return result;
+    }
+};
